0b9c2ca3f77e49b92db05749308a0370b221fad5
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2020-10-12 22:23:20 +0800

    寫好Graph的class

--- HW2/hw2 Maze/hw2 Maze.cpp
+++ HW2/hw2 Maze/hw2 Maze.cpp
@@ -1,11 +1,56 @@
 ﻿// hw2 Maze.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
 #include <iostream>
+#include <list>
+using namespace std;
+
+/* Adjacency List C++ */
+class Graph
+{
+    int numVertices;
+    list<int>* adjLists;
+
+    public:
+        Graph(int vertices);
+        void addEdge(int src, int dest);
+        void BFS(int startVertex);
+        void printGraph();
+};
+
+// Create a graph with given vertices,
+// and maintain an adjacency list
+Graph::Graph(int vertices) {
+    numVertices = vertices;
+    adjLists = new list<int>[vertices];
+}
+
+// Add edges to the graph
+void Graph::addEdge(int src, int dest) {
+    adjLists[src].push_back(dest);
+    adjLists[dest].push_back(src);
+}
+
+// Print the graph
+void Graph::printGraph() {
+    for (int d = 0; d < numVertices; ++d) {
+        cout << "\n Vertex "
+            << d << ":";
+        for (auto x : adjLists[d])
+            cout << "-> " << x;
+        printf("\n");
+    }
+}
 
 int main()
 {
     std::cout << "Hello World!\n";
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    //g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.printGraph();
+    system("pause");
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
