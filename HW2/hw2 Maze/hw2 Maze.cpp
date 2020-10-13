612a74c536ecfd51d49526960479ff946cfca914
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2020-10-13 12:38:03 +0800

    可以解析字串 存成大地圖

--- HW2/hw2 Maze/hw2 Maze.cpp
+++ HW2/hw2 Maze/hw2 Maze.cpp
@@ -1,6 +1,7 @@
 ﻿// hw2 Maze.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 #include <iostream>
 #include <list>
+#include <vector> 
 using namespace std;
 
 /* Adjacency List C++ */
@@ -15,6 +16,9 @@ class Graph
         void BFS(int startVertex);
         void printGraph();
 };
+
+/* Convert the maze(string) into Adjacency List */
+Graph parseString(int width, int height);
 
 // Create a graph with given vertices,
 // and maintain an adjacency list
@@ -42,17 +46,56 @@ void Graph::printGraph() {
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    //g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.printGraph();
+    //std::cout << "Hello World!\n";
+    int width, height;
+    cin >> width;
+    cin >> height;
+    cout << height << " " << width << endl;
+
+    Graph g = parseString(width, height);
+
+    //Graph g(4);
+    //g.addEdge(0, 1);
+    //g.addEdge(0, 2);
+    //g.addEdge(1, 2);
+    ////g.addEdge(2, 0);
+    //g.addEdge(2, 3);
+    //g.printGraph();
     system("pause");
 }
 
+Graph parseString(int width, int height) {
+    // Read the input maze string, store with an extra large maze. (m+2) * (n+2) size.
+    vector<bool> maze;
+    for (int j = 0; j < width+2; j++) // wall at first row
+        maze.push_back(1);
+    for (int i = 0; i < height; i++) {
+        maze.push_back(1);
+        for (int j = 0; j < width; j++) {
+            int node; cin >> node;
+            maze.push_back(node);
+        }
+        maze.push_back(1);
+    }
+    for (int j = 0; j < width+2; j++) // wall at last row
+        maze.push_back(1);
+
+    //print
+    cout << "\nprint maze:";
+    for (int i = 0; i < (height+2) * (width+2); i++) {
+        if (i % (width+2) == 0)
+            cout << endl;
+        cout << maze[i] << " ";
+    }
+
+    return Graph(width*height);
+}
+/* test1
+3 3
+0 1 1
+0 1 1
+1 0 0
+*/
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
 
