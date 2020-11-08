5582ba7f186650d5d7376e4c5b937c69896c5fc1
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2020-11-08 20:55:51 +0800

    hw4 lazy transpose ok

--- HW4 Sparse Matrix Multiplication/HW4 Sparse Matrix Multiplication.cpp
+++ HW4 Sparse Matrix Multiplication/HW4 Sparse Matrix Multiplication.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <stdlib.h>     /* atoi */
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 // Stores a Non-zero element.
@@ -19,6 +20,7 @@ public:
     int row = 0, col = 0, value = 0;
     Vertex() {}
     Vertex(int row, int col, int value) { this->row = row; this->col = col; this->value = value; }
+    bool operator<(const Vertex &r) const;
 };
 
 // Stores a complete matrix, contains info and list of elems.
@@ -29,14 +31,21 @@ public:
     
     Matrix(int row, int col) {
         row_count = row;
-        col_count = row;
+        col_count = col;
     }
-    Matrix fast_transpose();
+    // T-transform, as row major order. Return result.
+    Matrix lazyTranspose();
+    Matrix fastTranspose(); 
+    // Perform matrix multiplication algorithm. Return result.
+    Matrix multiplication(Matrix another);
+    // Print matrix's non-zero elements only.
+    void print(); 
+    // Print in n*m matrix format.
+    void prettyPrint();
 };
 
-Matrix ParseInputMatrix(int row, int col); // Abstract the matrix's non-zero elements. Row major.
-Matrix MatrixMultiplication(Matrix *a, Matrix *b); // Perform multiplication algorithm.
-void PrintMatrix(Matrix); // Print matrix's non-zero elements.
+// Abstract the matrix's non-zero elements. Row major.
+Matrix ParseInputMatrix(int row, int col); 
 
 int main()
 {
@@ -44,26 +53,63 @@ int main()
     cin >> row >> col;
     Matrix m1 = ParseInputMatrix(row, col);
 
-    cin >> row >> col;
-    Matrix m2 = ParseInputMatrix(row, col);
+   // cin >> row >> col;
+   // Matrix m2 = ParseInputMatrix(row, col);
+
+    // testing T-transpose
+    Matrix trans = m1.lazyTranspose();
+    cout << "\n";
+    trans.print();
+
+    // Matrix result = m1.multiplication(m2);
+    // result.print();
 }
 
 Matrix ParseInputMatrix(int row, int col) {
     Matrix m(row, col); // declare and init
-    char * input = new char[5];
+    char * input = new char[10];
     for (int r = 0; r < row; r++) {
         for (int c = 0; c < col; c++) {
             // if non-zero element, pushback & valcount++
             cin >> input;
-            int value = atoi(input);
-            if (value != 0) {
+            int value = atoi(input); // convert string to int
+            if (value != 0) 
                 m.list.push_back(Vertex(r, c, value));
-                m.val_count++;
-            }
         }
     }
+    m.val_count = m.list.size();
     return m;
 }
+
+// over load the operator<, compare 'row' , if equal then compare 'col'
+bool Vertex::operator<(const Vertex& r) const {
+    if (row != r.row)
+        return row < r.row;
+    return col < r.col;
+}
+
+void Matrix::print() {
+    for (auto v : this->list)
+        cout << v.row << " " << v.col << " " << v.value << endl;
+}
+
+Matrix Matrix::lazyTranspose() {
+    // init transpose matrix's header
+    Matrix trans(this->col_count, this->row_count);
+    trans.val_count = this->val_count;
+
+    // swap its row & col 
+    for (auto v : this->list) {
+        Vertex newv(v.col, v.row, v.value); // swap
+        trans.list.push_back(newv);
+    }
+
+    // sort by row then col, make sure it's row major order
+    std::sort(trans.list.begin(), trans.list.end());
+    return trans;
+}
+
+
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
