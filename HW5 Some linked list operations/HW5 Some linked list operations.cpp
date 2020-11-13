08f947a3cea0781800b53c54b3fab0605bc44e81
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2020-11-13 22:27:55 +0800

    hw5 insert OK

--- HW5 Some linked list operations/HW5 Some linked list operations.cpp
+++ HW5 Some linked list operations/HW5 Some linked list operations.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+using namespace std;
 
 struct node{
     int data;
@@ -16,11 +17,13 @@ public:
     LinkList();
     int size();
     int find(int value);
+    // Traverse and print all elements by space
+    void print();
     // Add element at the end of linklist
     void pushBack(int value);
-    // Insert element at given position, throw error messege while index out of bound
+    // Insert element at given position, throw error messege while index out of bound (first elem's index is 1)
     void insert(int index, int value);
-    // Delete element at given position, throw error messege while index out of bound
+    // Delete element at given position, throw error messege while index out of bound (first elem's index is 1)
     void deleteById(int index);
     // Invert the whole linklist
     void reverse();
@@ -29,9 +32,12 @@ public:
 
 int main()
 {
-    std::cout << "Hello World!\n";
-
-    LinkList list();
+    LinkList list;
+    list.pushBack(1);
+    list.pushBack(2);
+    list.pushBack(3);
+    list.insert(4, 999);
+    list.print();
 }
 
 LinkList::LinkList() {
@@ -39,9 +45,91 @@ LinkList::LinkList() {
     tail = NULL;
 }
 
-void LinkList::pushBack(int value) {
-
+void LinkList::print() {
+    node* current = this->head;
+    while (current) {
+        cout << current->data << " ";
+        current = current->next;
+    }
 }
+
+int LinkList::size() {
+    int size = 0;
+    node* current = this->head;
+    while (current) {
+        size++;
+        current = current->next;
+    }
+    return size;
+}
+
+void LinkList::pushBack(int value) {
+    // init new node
+    node *tmp = new node;
+    tmp->data = value;
+    tmp->next = NULL;
+
+    if (head == NULL) 
+        // First node, head & tail both point to first node
+        tail = head = tmp;
+    else {
+        // Not empty, tmp is the last node; 
+        tail->next = tmp; // (n-1) -> n
+        tail = tail->next;
+    }
+}
+
+void LinkList::insert(int index, int value) {
+    if (!index) {
+        // if index is 0, then insert as first node 
+        node* newn = new node;
+        newn->data = value;
+        newn->next = head; // new node point to the origin head node
+        head = newn;
+    }
+    else {
+        // newn is the new node, insert after target node
+        node* newn = new node;
+        newn->data = value;
+        node* target = new node;
+        target = head; // target point to first elem
+        // find the target node with value, throw index out of bound error
+        try {
+            int length = this->size(); // O(n)
+            if (index > length)
+                throw std::out_of_range("Index is out of range!!");
+            for (int i = 1; i < index; i++)
+                    target = target->next;
+        }
+        catch (std::exception& e) {
+            cout << e.what() << endl; // Problem requires: error message + original list
+            this->print();
+        }
+        // inserting a node 'newn' after node 'target'
+        newn->next = target->next;
+        target->next = newn;
+    }
+}
+
+/* Test Set 01
+ADD 5
+ADD 10
+ADD 90
+INSERT 11 BEHIND 1
+DELETE 4
+INSERT 9 BEHIND 5
+REVERSE
+*/
+
+/* Ans 01
+5
+5 10
+5 10 90
+5 11 10 90
+5 11 10
+Index is out of range!!
+5 11 10
+10 11 5*/
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
