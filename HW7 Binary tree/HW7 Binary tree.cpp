bd9b9ba7a39f979af110946c3ad46648b4280ba5
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2020-12-16 22:36:52 +0800

    HW7 AC

--- HW7 Binary tree/HW7 Binary tree.cpp
+++ HW7 Binary tree/HW7 Binary tree.cpp
@@ -1,9 +1,13 @@
 ﻿// HW7 Binary tree.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+// @Author: MIS Senior - Weber Lu
 
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
 using namespace std;
 
 const string EMPTY_TREE("Tree is empty");
@@ -12,8 +16,8 @@ const string INVALID_TREE("Invalid tree");
 class node {
 public:
     int val;
-    struct node* left;
-    struct node* right;
+    node* left;
+    node* right;
     // Construtor: New node creation
     node(int _v) : val(_v), left(nullptr), right(nullptr) {};
 
@@ -33,12 +37,138 @@ void traverseInOrder(node* temp);
 // Traverse Postorder
 void traversePostOrder(node* temp);
 
+// Recursive function to calculate height of given binary tree. Complexity : O(n)
+int height(node* root);
+
+// Calculate amounts of leaves. Complexity : O(n)
+int leafCount(node* root);
+
 int main()
 {
-    struct node* root = new node(1);
+    /* Test Code
+    node* root = new node(1);
     root->left = new node(2);
     root->right = new node(3);
-    root->left->left = new node(4);
+    root->left->left = new node(4); 
+    node* find = root->search(2);
+    if (find)
+        cout << find->val;
+    else
+        cout << INVALID_TREE;
+    int treeHeight = height(root);
+    int leatCount = leafCount(root);
+    cout << "treeHeight: " << treeHeight << ", leatCount: " << leatCount << endl;*/
+
+    node* root = NULL; // root node of tree
+    bool firstTime = true;
+
+    // build the binary tree
+    while (true) {
+
+        // read the input command. eg. "0 1 2"
+        string command;
+        getline(cin, command);
+        istringstream iss(command);
+        vector<string> tokens {
+            istream_iterator<string>{iss},
+            istream_iterator<string>{}
+        };
+
+        // end of file
+        if (tokens.size() == 0) 
+            break;
+
+        // target means which node to create sub-tree
+        int target = stoi(tokens[0]), left = stoi(tokens[1]), right = stoi(tokens[2]);
+
+        // adding childs, search for the target to create sub-tree, if not find throws error msg
+        if (firstTime) {
+            firstTime = false;
+            root = new node(target);
+            root->createLeaf(left, right);
+        }
+        else {
+            node* find = root->search(target);
+            if (find)
+                find->createLeaf(left, right);
+                // cout << find->val;
+            else {
+                cout << INVALID_TREE;
+                return 0;
+            }
+        }
+    } // end while
+
+    // print the tree height and amount of leaves
+    if (root == NULL)
+        cout << EMPTY_TREE;
+    else {
+        int treeHeight = height(root);
+        int leatCount = leafCount(root);
+        // cout << "treeHeight: " << treeHeight << ", leatCount: " << leatCount << endl;
+        cout << treeHeight << endl << leatCount << endl;
+    }
+}
+
+int height(node* root) {
+    // Base case: empty tree has height 0
+    if (root == nullptr)
+        return 0;
+
+    // recur for left and right subtree and consider maximum depth
+    return 1 + max(height(root->left), height(root->right));
+}
+
+int leafCount(node* root) {
+    // Base case: if node is null, return 
+    if (!root)
+        return 0;
+
+    // if node is leaf node, print its data     
+    if (!root->left && !root->right)
+        return 1;
+
+    // Recursive  case: add leaf counts of left child and right child
+    return leafCount(root->left) + leafCount(root->right);
+}
+
+void traversePreOrder(node* temp) {
+    if (temp == NULL)
+        return;
+
+    /* first print data of node */
+    cout << temp->val << " ";
+
+    traversePreOrder(temp->left);
+    traversePreOrder(temp->right);
+}
+
+void node::createLeaf(int left, int right) {
+    if (left)
+        this->left = new node(left);
+    if (right)
+        this->right = new node(right);
+}
+
+node* node::search(int value) {
+    // Complexity : O(n). Need to traverse nearly whole tree, same as DFS, BFS
+    // Base Cases: root is null or key is present at root (find the value!) (root means current node)
+    if (this == NULL)
+        return NULL;
+    else if (this->val == value)
+        return this;
+    
+    // Recursive Cases: if find in subtree return the target *ptr
+    else {
+        node* leftSubtreeResult = this->left->search(value);
+        node* rightSubtreeResult = this->right->search(value);
+        if (leftSubtreeResult)
+            return leftSubtreeResult;
+        else if (rightSubtreeResult)
+            return rightSubtreeResult;
+        else
+            return NULL;
+    }
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
---
+++ Iterate words of a string/Source.cpp
@@ -0,0 +1,40 @@
+// https://stackoverflow.com/questions/236129/how-do-i-iterate-over-the-words-of-a-string
+/*
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+*/
+// Split string
+/*
+int main() {
+    using namespace std;
+    string sentence = "And I feel fine...";
+    istringstream iss(sentence);
+    copy(istream_iterator<string>(iss),
+        istream_iterator<string>(),
+        ostream_iterator<string>(cout, "\n"));
+}
+*/
+
+// Via cin
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+int main() {
+    using namespace std;
+    string sentence;
+    getline(cin, sentence);
+    istringstream iss(sentence);
+    vector<string> tokens { 
+        istream_iterator<string>{iss},
+        istream_iterator<string>{} 
+    };
+
+}
