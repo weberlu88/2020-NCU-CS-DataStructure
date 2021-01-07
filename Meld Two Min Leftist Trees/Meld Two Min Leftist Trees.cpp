9fa8cec7f8bb6273f39d27d5aa10a3147f982a45
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2021-01-07 23:05:46 +0800

    Basic Operation done. Merge & Parse left.

--- Meld Two Min Leftist Trees/Meld Two Min Leftist Trees.cpp
+++ Meld Two Min Leftist Trees/Meld Two Min Leftist Trees.cpp
@@ -1,11 +1,129 @@
 ﻿// Meld Two Min Leftist Trees.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+// Data Structure https://www.geeksforgeeks.org/leftist-tree-leftist-heap/
+// Parse Array-style Heap into Tree-style Heap https://www.youtube.com/watch?v=t0Cq6tVNRBA
+// Merge Algorithm https://en.wikipedia.org/wiki/Leftist_tree
 
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class LeftistNode {
+public:
+    int element; // priority
+    int dist; // s-value, shortest path from the root to an external node
+    LeftistNode* leftChild, * rightChild; // leftchild & rightchild
+    LeftistNode(int& element, LeftistNode* lt = NULL, LeftistNode* rt = NULL, int np = 0){
+        this->element = element;
+        rightChild = rt;
+        leftChild = lt,
+        dist = np;
+    }
+};
+
+class LeftistHeap {
+private:
+    LeftistNode* root;
+    // Internal method to merge two roots.
+    LeftistNode* Merge(LeftistNode* h1, LeftistNode* h2);
+    // Internal method to swap node's two children. 
+    void swapChildren(LeftistNode* t);
+
+    /* Deal with Heap transformation between 'Array-style' & 'Tree-style'. */
+    int capacity = 0;
+    vector<int> items;
+    int getRightChildIndex(int parentIndex) { return parentIndex * 2 + 2; }
+    int getLeftChildIndex(int parentIndex) { return parentIndex * 2 + 1; }
+    int getParentIndex(int childIndex) { return (childIndex - 1) / 2; } // auto flooring
+
+    bool hasRightChild(int index) { return getRightChildIndex(index) > capacity; }
+    bool hasLeftChild(int index) { return getLeftChildIndex(index) > capacity; }
+    bool hasParent(int index) { return getParentIndex(index) >= 0; }
+
+    int rightChild(int index) { return items[getRightChildIndex(index)]; }
+    int leftChild(int index) { return items[getLeftChildIndex(index)]; }
+    int parent(int index) { return items[getParentIndex(index)]; }
+
+public:
+    // Construct empty leftist heap 
+    LeftistHeap();
+    // Construct the leftist heap with input array
+    LeftistHeap(vector<int> input);
+    // Copy constructor. 
+    LeftistHeap(LeftistHeap& rhs);
+    /* Merge rhs into the priority queue. rhs becomes empty. rhs must be different from this.*/
+    void Merge(LeftistHeap& rhs);
+    /* Print with 'Array-style'.*/
+    void printArrayStyle();
+    /* Print with 'Tree-style'.*/
+    void printTreeStyle();
+};
+
+// Construct empty leftist heap 
+LeftistHeap::LeftistHeap()
+{
+    root = NULL;
+}
+
+// Construct the leftist heap with input array
+LeftistHeap::LeftistHeap(vector<int> input)
+{
+    root = NULL;
+    return;
+}
+
+// Copy constructor. 
+LeftistHeap::LeftistHeap(LeftistHeap& rhs)
+{
+    root = NULL;
+    *this = rhs;
+}
+
+/* Merge rhs into the priority queue. rhs becomes empty. rhs must be different from this.*/
+void LeftistHeap::Merge(LeftistHeap& rhs)
+{
+    if (this == &rhs)
+        return;
+    root = Merge(root, rhs.root); // merge itself with rhs
+    rhs.root = NULL;
+}
+
+// Internal method to merge two roots.
+LeftistNode* LeftistHeap::Merge(LeftistNode* h1, LeftistNode* h2)
+{
+    return NULL;
+}
+
+// Internal method to swap node's two children. 
+void LeftistHeap::swapChildren(LeftistNode* t)
+{
+    LeftistNode* temp = t->leftChild;
+    t->leftChild = t->rightChild;
+    t->rightChild = temp;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int size1, size2;
+    cin >> size1 >> size2;
+
+    int priority;
+    string string1, string2;
+    for (int i = 1; i <= size1; ++i) {
+        cin >> priority;
+        string1 += priority;
+    }
+    for (int i = 1; i <= size2; ++i) {
+        cin >> priority;
+        string2 += priority;
+    }
+
+    cout << string1.size() << endl;
+    cout << string2.size() << endl;
+
+    // LeftistHeap heap1;
+    // LeftistHeap heap2;
+
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
